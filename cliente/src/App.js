0ed3e7cc1baaf1c75f406d93dd9b8b7205a5779b
Lucas <lucasmcoronel@gmail.com>
2023-03-28 22:38:17 -0300

    generacion de rutas

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
-import ListaUsuarios from './components/home/ListaUsuarios';
-import AgregarUsuarios from './components/home/AgregarUsuarios';
-import EditarUsuario from './components/home/EditarUsuarios';
+import ListaUsuarios from './components/pages/ListaUsuarios'
+import AgregarUsuarios from './components/pages/AgregarUsuarios';
+import EditarUsuario from './components/pages/EditarUsuarios';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <h1> Crud MERN STACK </h1>
-      <ListaUsuarios/>
-      <AgregarUsuarios/>
-      <EditarUsuario/>
+
+      <BrowserRouter>
+        <Routes>
+        
+            <Route exact path='/' element={<ListaUsuarios />}></Route>
+            <Route exact path='/AgregarUsuarios' element={<AgregarUsuarios />}></Route>
+            <Route exact path='/EditarUsuarios' element={<EditarUsuario />}></Route>
+
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
